e9e16b427d76b95575f3f32a980382c6e68a251b
canbulgay <canbulgay@outlook.com>
2022-01-09 22:37:27 +0300

    Created non-dynamic posts in resource,Change routes to find posts

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+
+<body>
+
+<article>
+    <h1><a href="/posts/my-first-post">My First Post</a></h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique facilis obcaecati quos dolore ratione eveniet vitae deleniti! Laudantium aut, alias assumenda in minus tempore omnis.</p>
+</article>    
+<article>
+    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique facilis obcaecati quos dolore ratione eveniet vitae deleniti! Laudantium aut, alias assumenda in minus tempore omnis.</p>
+</article>    
+<article>
+    <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique facilis obcaecati quos dolore ratione eveniet vitae deleniti! Laudantium aut, alias assumenda in minus tempore omnis.</p>
+</article>    
+
+</body>
