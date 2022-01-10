3693c1c5b9bf451f3e572ca058c83ca1912647ce
canbulgay <canbulgay@outlook.com>
2022-01-10 15:13:32 +0300

    İnstall clockwork for debuging to sql queries, make relationship users and posts

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,14 +2,14 @@
 @section('content')
     <article>
 
-        <h1>{{ $post->title }}</h1>
+        <h1>{{!! $post->title !!}}</h1>
         
         <p>
-            <a href="">{{$post->category->name}}</a>
+            By <a href="#">{{$post->user->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
         </p>
         
         <div>
-            {{ $post->body }}
+            {{!! $post->body !!}}
         </div>
 
     
