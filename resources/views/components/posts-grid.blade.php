42ef8cc37e583322a704fd974b9eda717317f19d
canbulgay <canbulgay@outlook.com>
2022-01-12 15:20:39 +0300

    Convert to Post Page

--- resources/views/components/posts-grid.blade.php
+++ resources/views/components/posts-grid.blade.php
@@ -5,7 +5,7 @@
     <div class="lg:grid lg:grid-cols-6">
         @foreach ($posts as $post)
         <x-post-card 
-        :post"$post" 
+        :post="$post" 
         class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}"
         />
         @endforeach
