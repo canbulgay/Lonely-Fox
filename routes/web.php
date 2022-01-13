ce741cec62407ccfc9209d832bfcb24df9c75057
canbulgay <canbulgay@outlook.com>
2022-01-13 22:34:20 +0300

    Modified posts routes and make controllers to process posts

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
@@ -16,30 +17,9 @@ use App\Models\User;
 |
 */
 
-Route::get('/', function () {
+Route::get('/',[PostController::class,'index'])->name('home');
 
-    $posts = Post::latest();
-    if(request('search')){
-        $posts->where('title','like','%'.request('search').'%')
-        ->orWhere('body','like','%'.request('search').'%');
-    }
-
-    return view('posts',[
-    'posts' => $posts,
-    'categories' => Category::all(),
-    ]);
-})->name('home');
-
-Route::get('posts/{post:slug}',function(Post $post){
-
-    /*
-    ! Find a post by its slug and pass it to a view called "post"
-    */
-    return view('post',[
-
-        'post' => $post
-    ]);
-});
+Route::get('posts/{post:slug}',[PostController::class,'show']);
 
 Route::get('categories/{category:slug}', function(Category $category){
 
